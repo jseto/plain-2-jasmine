97d805a7c63c11989e05beb27d33b77602e5aff9
Josep Seto <jsetojseto@gmail.com>
2015-11-16 21:12:26 +0100

    accepts CRLF line endings
    closes #4

--- spec/insert-jasmine-syntax-spec.js
+++ spec/insert-jasmine-syntax-spec.js
@@ -3,191 +3,220 @@
 var insertJasmineSyntax = require('../lib/insert-jasmine-syntax.js');
 
 describe( 'insertJasmineSyntax module', function(){
-    it( 'should insert in unindent it after same level describe block pattern', function(){
-        var buffer = [
-            'the first describe',
-            '    a description again',
-            '        another it',
-            '    another it',
-            '    a description',
-            '        an it'
-        ].join('\n');
+	it( 'should insert in unindent it after same level describe block pattern', function(){
+		var buffer = [
+			'the first describe',
+			'    a description again',
+			'        another it',
+			'    another it',
+			'    a description',
+			'        an it'
+		].join('\n');
 
-        var expectation = [
-            'describe( \'the first describe\', function() {',
-            '    describe( \'a description again\', function() {',
-            '        it( \'another it\', function() {',
-            '        });',
-            '    });',
-            '    it( \'another it\', function() {',
-            '    });',
-            '    describe( \'a description\', function() {',
-            '        it( \'an it\', function() {',
-            '        });',
-            '    });',
-            '});'
-        ].join('\n');
+		var expectation = [
+			'describe( \'the first describe\', function() {',
+			'    describe( \'a description again\', function() {',
+			'        it( \'another it\', function() {',
+			'        });',
+			'    });',
+			'    it( \'another it\', function() {',
+			'    });',
+			'    describe( \'a description\', function() {',
+			'        it( \'an it\', function() {',
+			'        });',
+			'    });',
+			'});'
+		].join('\n');
 
-        expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
-    });
+		expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
+	});
 
-    it( 'should insert in unindent pattern', function(){
-        var buffer = [
-            'the first describe',
-            '    a description again',
-            '        another it',
-            '    a description',
-            '        an it',
-            'other main description',
-            '    a sub-description',
-            '        an it here'
-        ].join('\n');
+	it( 'should insert in unindent pattern', function(){
+		var buffer = [
+			'the first describe',
+			'    a description again',
+			'        another it',
+			'    a description',
+			'        an it',
+			'other main description',
+			'    a sub-description',
+			'        an it here'
+		].join('\n');
 
-        var expectation = [
-            'describe( \'the first describe\', function() {',
-            '    describe( \'a description again\', function() {',
-            '        it( \'another it\', function() {',
-            '        });',
-            '    });',
-            '    describe( \'a description\', function() {',
-            '        it( \'an it\', function() {',
-            '        });',
-            '    });',
-            '});',
-            'describe( \'other main description\', function() {',
-            '    describe( \'a sub-description\', function() {',
-            '        it( \'an it here\', function() {',
-            '        });',
-            '    });',
-            '});'
-        ].join('\n');
+		var expectation = [
+			'describe( \'the first describe\', function() {',
+			'    describe( \'a description again\', function() {',
+			'        it( \'another it\', function() {',
+			'        });',
+			'    });',
+			'    describe( \'a description\', function() {',
+			'        it( \'an it\', function() {',
+			'        });',
+			'    });',
+			'});',
+			'describe( \'other main description\', function() {',
+			'    describe( \'a sub-description\', function() {',
+			'        it( \'an it here\', function() {',
+			'        });',
+			'    });',
+			'});'
+		].join('\n');
 
-        expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
-    });
+		expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
+	});
 
-    it( 'should not include blank lines', function(){
-        var buffer = [
-            'a trivial buffer',
-            '\tshould return proper jasmine',
-            ''
-        ].join('\n');
+	it( 'should not include blank lines', function(){
+		var buffer = [
+			'a trivial buffer',
+			'\tshould return proper jasmine',
+			''
+		].join('\n');
 
-        var expectation = [
-            'describe( \'a trivial buffer\', function() {',
-            '\tit( \'should return proper jasmine\', function() {',
-            '\t});',
-            '});'
-        ].join('\n');
+		var expectation = [
+			'describe( \'a trivial buffer\', function() {',
+			'\tit( \'should return proper jasmine\', function() {',
+			'\t});',
+			'});'
+		].join('\n');
 
-        expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
-        buffer += '    ';
-        expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
-    });
+		expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
+		buffer += '    ';
+		expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
+	});
 
-    it( 'should insert jasmine syntax in a trivial buffer with hard-tabs', function(){
-        var buffer = [
-            'a trivial buffer',
-            '\tshould return proper jasmine'
-        ].join('\n');
+	it( 'should insert jasmine syntax in a trivial buffer with hard-tabs', function(){
+		var buffer = [
+			'a trivial buffer',
+			'\tshould return proper jasmine'
+		].join('\n');
 
-        var expectation = [
-            'describe( \'a trivial buffer\', function() {',
-            '\tit( \'should return proper jasmine\', function() {',
-            '\t});',
-            '});'
-        ].join('\n');
+		var expectation = [
+			'describe( \'a trivial buffer\', function() {',
+			'\tit( \'should return proper jasmine\', function() {',
+			'\t});',
+			'});'
+		].join('\n');
 
-        expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
-    });
+		expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
+	});
 
-    it( 'should insert jasmine syntax in a trivial buffer with soft-tabs', function(){
-        var buffer = [
-            'a trivial buffer',
-            '  should return proper jasmine'
-        ].join('\n');
+	it( 'should insert jasmine syntax in a trivial buffer with soft-tabs', function(){
+		var buffer = [
+			'a trivial buffer',
+			'  should return proper jasmine'
+		].join('\n');
 
-        var expectation = [
-            'describe( \'a trivial buffer\', function() {',
-            '  it( \'should return proper jasmine\', function() {',
-            '  });',
-            '});'
-        ].join('\n');
+		var expectation = [
+			'describe( \'a trivial buffer\', function() {',
+			'  it( \'should return proper jasmine\', function() {',
+			'  });',
+			'});'
+		].join('\n');
 
-        expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
-    });
+		expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
+	});
 
-    it( 'should insert in describe-decribe-it pattern', function(){
-        var buffer = [
-            'the first describe',
-            '   should be followed by a second describe',
-            '       and an it function'
-        ].join('\n');
+	it( 'should insert in describe-decribe-it pattern', function(){
+		var buffer = [
+			'the first describe',
+			'   should be followed by a second describe',
+			'       and an it function'
+		].join('\n');
 
-        var expectation = [
-            'describe( \'the first describe\', function() {',
-            '   describe( \'should be followed by a second describe\', function() {',
-            '       it( \'and an it function\', function() {',
-            '       });',
-            '   });',
-            '});'
-        ].join('\n');
+		var expectation = [
+			'describe( \'the first describe\', function() {',
+			'   describe( \'should be followed by a second describe\', function() {',
+			'       it( \'and an it function\', function() {',
+			'       });',
+			'   });',
+			'});'
+		].join('\n');
 
-        expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
-    });
+		expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
+	});
 
-    it( 'should insert in describe-it-decribe-it pattern', function(){
-        var buffer = [
-            'the first describe',
-            '   should be followed by it',
-            '   and an a description',
-            '       and other it'
-        ].join('\n');
+	it( 'should insert in describe-it-decribe-it pattern', function(){
+		var buffer = [
+			'the first describe',
+			'   should be followed by it',
+			'   and an a description',
+			'       and other it'
+		].join('\n');
 
-        var expectation = [
-            'describe( \'the first describe\', function() {',
-            '   it( \'should be followed by it\', function() {',
-            '   });',
-            '   describe( \'and an a description\', function() {',
-            '       it( \'and other it\', function() {',
-            '       });',
-            '   });',
-            '});'
-        ].join('\n');
+		var expectation = [
+			'describe( \'the first describe\', function() {',
+			'   it( \'should be followed by it\', function() {',
+			'   });',
+			'   describe( \'and an a description\', function() {',
+			'       it( \'and other it\', function() {',
+			'       });',
+			'   });',
+			'});'
+		].join('\n');
 
-        expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
-    });
+		expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
+	});
 
-    it( 'should insert in describe-it-it-decribe-it-describe-it-it pattern', function(){
-        var buffer = [
-            'the first describe',
-            '   should be followed by it',
-            '   should be followed by another it',
-            '   and an a description',
-            '       and other it',
-            '       a description again',
-            '           another it',
-            '           and one more it'
-        ].join('\n');
+	it( 'should insert in describe-it-it-decribe-it-describe-it-it pattern', function(){
+		var buffer = [
+			'the first describe',
+			'   should be followed by it',
+			'   should be followed by another it',
+			'   and an a description',
+			'       and other it',
+			'       a description again',
+			'           another it',
+			'           and one more it'
+		].join('\n');
 
-        var expectation = [
-            'describe( \'the first describe\', function() {',
-            '   it( \'should be followed by it\', function() {',
-            '   });',
-            '   it( \'should be followed by another it\', function() {',
-            '   });',
-            '   describe( \'and an a description\', function() {',
-            '       it( \'and other it\', function() {',
-            '       });',
-            '       describe( \'a description again\', function() {',
-            '           it( \'another it\', function() {',
-            '           });',
-            '           it( \'and one more it\', function() {',
-            '           });',
-            '       });',
-            '   });',
-            '});'
-        ].join('\n');
+		var expectation = [
+			'describe( \'the first describe\', function() {',
+			'   it( \'should be followed by it\', function() {',
+			'   });',
+			'   it( \'should be followed by another it\', function() {',
+			'   });',
+			'   describe( \'and an a description\', function() {',
+			'       it( \'and other it\', function() {',
+			'       });',
+			'       describe( \'a description again\', function() {',
+			'           it( \'another it\', function() {',
+			'           });',
+			'           it( \'and one more it\', function() {',
+			'           });',
+			'       });',
+			'   });',
+			'});'
+		].join('\n');
 
-        expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
-    });
+		expect( insertJasmineSyntax( buffer ).join('\n') ).toBe( expectation );
+	});
+
+	it( 'should work for windows line endings', function(){
+		var buffer = [
+			'the first describe',
+			'    a description again',
+			'        another it',
+			'    another it',
+			'    a description',
+			'        an it'
+		].join('\r\n');
+
+		var expectation = [
+			'describe( \'the first describe\', function() {',
+			'    describe( \'a description again\', function() {',
+			'        it( \'another it\', function() {',
+			'        });',
+			'    });',
+			'    it( \'another it\', function() {',
+			'    });',
+			'    describe( \'a description\', function() {',
+			'        it( \'an it\', function() {',
+			'        });',
+			'    });',
+			'});'
+		].join('\r\n');
+
+		expect( insertJasmineSyntax( buffer, '\r\n' ).join('\r\n') ).toBe( expectation );
+	});
+
 });
--- spec/plain-2-jasmine-spec.js
+++ spec/plain-2-jasmine-spec.js
@@ -3,50 +3,50 @@
 var Plain2Jasmine = require('../lib/plain-2-jasmine');
 
 xdescribe('Plain2Jasmine', function() {
-    var activationPromise, ref, workspaceElement;
-    ref = [], workspaceElement = ref[0], activationPromise = ref[1];
+	var activationPromise, ref, workspaceElement;
+	ref = [], workspaceElement = ref[0], activationPromise = ref[1];
 
-    beforeEach(function() {
-        workspaceElement = atom.views.getView(atom.workspace);
-        activationPromise = atom.packages.activatePackage('plain-2-jasmine');
-    });
+	beforeEach(function() {
+		workspaceElement = atom.views.getView(atom.workspace);
+		activationPromise = atom.packages.activatePackage('plain-2-jasmine');
+	});
 
-    describe('when the plain-2-jasmine:toggle event is triggered', function() {
+	describe('when the plain-2-jasmine:toggle event is triggered', function() {
 
-        it('hides and shows the modal panel', function() {
-            expect(workspaceElement.querySelector('.plain-2-jasmine')).not.toExist();
-            atom.commands.dispatch(workspaceElement, 'plain-2-jasmine:toggle');
-            waitsForPromise(function() {
-                activationPromise;
-            });
-            runs(function() {
-                var plain2JasmineElement, plain2JasminePanel;
+		it('hides and shows the modal panel', function() {
+			expect(workspaceElement.querySelector('.plain-2-jasmine')).not.toExist();
+			atom.commands.dispatch(workspaceElement, 'plain-2-jasmine:toggle');
+			waitsForPromise(function() {
+				activationPromise;
+			});
+			runs(function() {
+				var plain2JasmineElement, plain2JasminePanel;
 
-                expect(workspaceElement.querySelector('.plain-2-jasmine')).toExist();
-                plain2JasmineElement = workspaceElement.querySelector('.plain-2-jasmine');
-                expect(plain2JasmineElement).toExist();
-                plain2JasminePanel = atom.workspace.panelForItem(plain2JasmineElement);
-                expect(plain2JasminePanel.isVisible()).toBe(true);
-                atom.commands.dispatch(workspaceElement, 'plain-2-jasmine:toggle');
+				expect(workspaceElement.querySelector('.plain-2-jasmine')).toExist();
+				plain2JasmineElement = workspaceElement.querySelector('.plain-2-jasmine');
+				expect(plain2JasmineElement).toExist();
+				plain2JasminePanel = atom.workspace.panelForItem(plain2JasmineElement);
+				expect(plain2JasminePanel.isVisible()).toBe(true);
+				atom.commands.dispatch(workspaceElement, 'plain-2-jasmine:toggle');
 
-                expect(plain2JasminePanel.isVisible()).toBe(false);
-            });
-        });
+				expect(plain2JasminePanel.isVisible()).toBe(false);
+			});
+		});
 
-        it('hides and shows the view', function() {
-            jasmine.attachToDOM(workspaceElement);
-            expect(workspaceElement.querySelector('.plain-2-jasmine')).not.toExist();
-            atom.commands.dispatch(workspaceElement, 'plain-2-jasmine:toggle');
-            waitsForPromise(function() {
-                activationPromise;
-            });
-            runs(function() {
-                var plain2JasmineElement;
-                plain2JasmineElement = workspaceElement.querySelector('.plain-2-jasmine');
-                expect(plain2JasmineElement).toBeVisible();
-                atom.commands.dispatch(workspaceElement, 'plain-2-jasmine:toggle');
-                expect(plain2JasmineElement).not.toBeVisible();
-            });
-        });
-    });
+		it('hides and shows the view', function() {
+			jasmine.attachToDOM(workspaceElement);
+			expect(workspaceElement.querySelector('.plain-2-jasmine')).not.toExist();
+			atom.commands.dispatch(workspaceElement, 'plain-2-jasmine:toggle');
+			waitsForPromise(function() {
+				activationPromise;
+			});
+			runs(function() {
+				var plain2JasmineElement;
+				plain2JasmineElement = workspaceElement.querySelector('.plain-2-jasmine');
+				expect(plain2JasmineElement).toBeVisible();
+				atom.commands.dispatch(workspaceElement, 'plain-2-jasmine:toggle');
+				expect(plain2JasmineElement).not.toBeVisible();
+			});
+		});
+	});
 });
